48308375383e2b8e4d9f4d7b95dcd2cbe8d13918
mohit <mohitchandola970@gmail.com>
2023-09-03 12:54:24 +0530

    make error.js file

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,6 +10,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+     next(error);
   }
 };
